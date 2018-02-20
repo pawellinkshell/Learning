12039a417706000a22cd764f4382809e5027ec5c
Jan Koszela <jan.koszela@eclipsegroup.co.uk>
2018-02-20 23:55:14 +0100

    Add commentary, add bean properties

--- src/pl/pawel/linkshell/learning/lesson_1/bean_properties/User.java
+++ src/pl/pawel/linkshell/learning/lesson_1/bean_properties/User.java
@@ -1,22 +1,20 @@
-package pl.pawel.linkshell.learning.lesson_1.pojo_aka_normal_properties;
+package pl.pawel.linkshell.learning.lesson_1.bean_properties;
+
+import java.io.Serializable;
 
 /*
- * POJO is an acronym for Plain Old Java Object. The name is used to emphasize
- * that the object in question is an ordinary Java Object,
- * not a special object, and in particular not an Enterprise JavaBean (especially before EJB 3),
- * not an Spring Bean, depend on 3-rd party dependency
+ * A JavaBean is a Java Object that:
+ *    * is serializable,
+ *    * has a nullary constructor,
+ *    and allows access to properties using getter and setter methods.
  */
-public class User {
+public class User implements Serializable{
 
   private String name;
   private String surname;
   private int age;
 
-  public User(String name, String surname, int age) {
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-  }
+  public User() {}
 
   public String getName() {
     return name;
@@ -54,7 +52,12 @@ public class User {
   public static void main(String[] args) {
     // 1) Getting inner fields of class using toString()
     System.out.println("Getting inner fields of class using toString()");
-    User user_1 = new User("Jeff", "Dummy", 27);
+    User user_1 = new User();
+
+    user_1.setName("Jeff");
+    user_1.setSurname("Dummy");
+    user_1.setAge(27);
+
     System.out.println(user_1);
 
   }
